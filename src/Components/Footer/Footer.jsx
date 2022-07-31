1f6f8ce9d9aca5ad767f02e57c83a13fba200e1b
Muhsin TT <muhsintt777@gmail.com>
2022-08-01 02:48:01 +0530

    added description and facebook icon in the footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import FacebookIcon from "../../assets/icons/FacebookIcon";
 import "./Footer.css";
 
 function Footer() {
   return (
     <div className="footer">
-      <p>footer</p>
+      <div className="footer_description">
+        <p>&#169; Green Commerce Private Limited 2016-2022</p>
+      </div>
+      <div className="footer_icons">
+        <FacebookIcon />
+      </div>
     </div>
   );
 }
